59a4cb55ab6d6078c1892403a058352c88d3e143
KatieMBerry <katieberrym@gmail.com>
2021-07-30 10:33:29 -0700

    animate attributes added to elements, initial, transition, whielHover, variants, yoyo added for animation effects

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity
+    }
+  }
+}
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        delay: 1.5,
+        duration: 1.5
+      }}>
+
+      <h2>Welcome to Pizza Joint</h2>
+
       <Link to="/base">
-        <button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+        // whileHover={{
+        //   // scale: 1.1,
+        //   // textShadow: "0px 0px 8px rgb(255,255,255)",
+        //   // boxShadow: "0px 0px 8px rgb(255,255,255)",
+        // }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
